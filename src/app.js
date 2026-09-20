ef7694d42f60ae27ff090b46c654fe83b4114416
Horbunov Yurii <111283552+Horbunov-Yurii@users.noreply.github.com>
2026-09-20 14:41:54 +0300

    first

---
+++ src/app.js
@@ -0,0 +1,7 @@
+console.log("Hello world");
+
+
+const names = ["Jura", "Pavlo", "Nata", "Tanua", "Olha"]
+
+const namesFiltered = names.filter((name) => name.startsWith("J"))
+console.log(namesFiltered);
